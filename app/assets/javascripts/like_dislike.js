d0a45f4b00b0036d92efcdc220809a9396b0e1fd
kapil kumar <kapil07517@gmail.com>
2014-12-04 15:07:24 +0530

    optimized some javascript code

--- app/assets/javascripts/like_dislike.js
+++ app/assets/javascripts/like_dislike.js
@@ -2,31 +2,27 @@ $(document).ready(function() {
 	$(".voting_wrapper .voting_btn").click(function (e) {
 		var clicked_button = $(this).children().attr('class');
 		var unique_id = $(this).parent().attr("id");
+		var like_state,msg;
 		if(clicked_button==='down_button')
 		{
-			post_data = {'id':unique_id, 'vote':'down'};
-			$.post('vote_items', post_data, function(data) {
-				getData = data.toString().split(",")
-				$('#'+unique_id+' .up_votes').text(getData[0]);
-				$('#'+unique_id+' .down_votes').text(getData[1]);
-			}).fail(function(err) {
-				if (err.statusText == "Unauthorized"){
-					alert("Please login to UnLike")
-				}
-			});
+			like_state = 'down';
+			msg = 'Please login to UnLike';
 		}
 		else if(clicked_button==='up_button')
 		{
-			post_data = {'id':unique_id, 'vote':'up'};
-			$.post('vote_items', post_data, function(data) {
-				getData = data.toString().split(",")
-				$('#'+unique_id+' .up_votes').text(getData[0]);
-				$('#'+unique_id+' .down_votes').text(getData[1]);
-			}).fail(function(err) {
-				if (err.statusText == "Unauthorized"){
-					alert("Please login to Like")
-				}
-			});
+			like_state = 'up';
+			msg = 'Please login to Like';			
 		}
+
+		post_data = {'id':unique_id, 'vote':like_state};
+		$.post('vote_items', post_data, function(data) {
+		  getData = data.toString().split(",")
+		  $('#'+unique_id+' .up_votes').text(getData[0]);
+		  $('#'+unique_id+' .down_votes').text(getData[1]);
+		}).fail(function(err) {
+		  if (err.statusText == "Unauthorized"){
+		    alert(msg)
+		  }
+		});
 	});
 });
